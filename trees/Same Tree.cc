1e2d755e152376362fe25be53eab2c31df5c7e60
Pointbr8ker-123 <nwosudavid13@gmail.com>
2026-07-11 03:30:31 +0100

    solve leetcode 100

--- trees/Same Tree.cc
+++ trees/Same Tree.cc
@@ -14,8 +14,10 @@ using namespace std;
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if (root == nullptr) return 0; // for leetcode solution... else should be -1
-        return max(maxDepth(root->right), maxDepth(root->left)) + 1;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
