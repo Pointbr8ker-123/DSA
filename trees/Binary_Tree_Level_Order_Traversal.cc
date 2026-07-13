c869dbc61c261bffd40cb6365bc73fab0d43557e
Pointbr8ker-123 <nwosudavid13@gmail.com>
2026-07-13 05:12:50 +0100

    solve leetcode 102

---
+++ trees/Binary_Tree_Level_Order_Traversal.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if (!root) return ans;
+
+        queue<TreeNode*> Q;
+        Q.push(root);
+        
+        while(!Q.empty()) {
+            int size = Q.size();
+
+            vector<int> children;
+            for (int i = 0; i < size; i++) {
+                TreeNode* curr = Q.front();
+                Q.pop();
+
+                children.push_back(curr->val);
+                if (curr->left) Q.push(curr->left);
+                if (curr->right) Q.push(curr->right);
+            }
+            ans.push_back(children);
+        }
+        return ans;
+    }
+};
