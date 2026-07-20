76ee94b9ce7a3adcdd7a82da3c409e2f0b7a0408
Pointbr8ker-123 <nwosudavid13@gmail.com>
2026-07-20 21:00:16 +0100

    solve leetcode 235 (Optimal solution)

---
+++ trees/Lowest_Common_Ancestor_of_a_Binary_Search_Tree(Optimal_Solution).cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // Basically, the solution is to apply the principle of Binary Search on
+        // the BST, using the root node as the "mid" element instead
+        while (root) {
+            if (p->val > root->val && q->val > root->val) {
+                root = root->right;
+            } else if (p->val < root->val && q->val < root->val) {
+                root = root->left;
+            } else {
+                return root;
+            }
+        }
+        return nullptr;
+    }
+};
