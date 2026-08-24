03d3090a1e7d549bb46819792abd297a4fc0837d
Pointbr8ker-123 <nwosudavid13@gmail.com>
2026-08-24 23:56:24 +0100

    solve leetcode 286

---
+++ graphs/286_Walls_and_Gates.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> map_gate_distances(vector<vector<int>>& dungeon_map) {
+    int rows = dungeon_map.size();
+    int cols = dungeon_map[0].size();
+    queue<pair<int,int>> q;
+
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            if (dungeon_map[r][c] == 0) {
+                q.push({r,c});
+            }
+        }
+    }
+
+    vector<pair<int, int>> dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+    int dist = 0;
+    while(!q.empty()) {
+        int n = q.size();
+        for (int i = 0; i < n; i++) {
+            auto[r,c] = q.front(); q.pop();
+            for (auto& [dr,dc] : dirs) {
+                int nr = dr + r, nc = dc + c;
+                if ((nr >= 0 && nr < rows) &&
+                    (nc >= 0 && nc < cols) &&
+                    dungeon_map[nr][nc] == INT_MAX) {
+                        dungeon_map[nr][nc] = dist + 1;
+                        q.push({nr, nc});
+                    }
+            }
+        }
+        dist++;
+    }
+    return dungeon_map;
+}
